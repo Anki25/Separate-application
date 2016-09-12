3df92b6d909f62bf7a4afa6b885ece53fbdf1b13
ANKITA MISHRA <mishrankita25@yahoo.com>
2016-09-12 23:52:46 +0530

    Add files via upload

--- ProductDAOImpl.java
+++ ProductDAOImpl.java
@@ -1,4 +1,4 @@
-package com.niit.shoppingcart.dao;
+package com.niit.dao;
 
 import java.util.List;
 
@@ -10,78 +10,111 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.niit.shoppingcart.model.Product;
+import com.niit.model.Product;
 
 @EnableTransactionManagement
 @Repository("productDAO")
 public class ProductDAOImpl implements ProductDAO {
 
-	@Autowired  //no need to create object of session factory ...automatically created
+	@Autowired // no need to create object of session factory ...automatically
+				// created
 	private SessionFactory sessionFactory;
-	
-	public ProductDAOImpl(SessionFactory sessionFactory){
-		this.sessionFactory = sessionFactory;		
+
+	public ProductDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
 	}
+
+	/*
+	 * @Transactional public boolean save(Product product) { // TODO
+	 * Auto-generated method stub try{
+	 * sessionFactory.getCurrentSession().save(product); return true;
+	 * }catch(Exception e){ e.printStackTrace(); return false; } }
+	 * 
+	 * @Transactional public boolean update(Product product) { // TODO
+	 * Auto-generated method stub try{
+	 * sessionFactory.getCurrentSession().update(product); return true;
+	 * }catch(Exception e){ e.printStackTrace(); return false; } }
+	 */
 	@Transactional
-	public boolean save(Product product) {
+	public boolean delete(Product product) { // since object is given
 		// TODO Auto-generated method stub
-		try{
-		sessionFactory.getCurrentSession().save(product);
-		return true;
-		}catch(Exception e){
+		try {
+			sessionFactory.getCurrentSession().delete(product);
+			return true;
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 	}
 
 	@Transactional
-	public boolean update(Product product) {
-		// TODO Auto-generated method stub
-		try{
-			sessionFactory.getCurrentSession().update(product);
-			return true;
-			}catch(Exception e){
-				e.printStackTrace();
-				return false;
-			}
+	public Product get(String id) {
+		// TODO Auto-generated method stub selct * from product where id = '101'
+		String hql = "from Product where id= " + "'" + id + "'"; // product
+																		// is d
+																		// domain
+																		// object
+																		// name
+																		// not
+																		// table
+																		// name
+
+		Query query = sessionFactory.getCurrentSession().createQuery(hql);
+		List<Product> list = query.list();
+
+		if (list == null || list.isEmpty()) {
+			return null;
+		} else {
+			return list.get(0);
+		}
 	}
 
 	@Transactional
-	public boolean delete(Product product) {  //since object is given 
-		// TODO Auto-generated method stub
-		try{
-			sessionFactory.getCurrentSession().delete(product);
-			return true;
-			}catch(Exception e){
-				e.printStackTrace();
-				return false;
-			}
-	}
-	
-			public Product get(String id) {
-		// TODO Auto-generated method stub  selct * from product where id = '101'
-		String hql = "from Product where id= " + " ' " + id + " ' ";  //product is d domain object name not table name
-		
-		Query query = sessionFactory.getCurrentSession().createQuery(hql);
-		List <Product> list = query.list();
-		
-		if(list==null || list.isEmpty())
-		{
-		return null;
-	}else
-	{ return list.get(0);
-	}
-	}
 	public List<Product> list() {
-		// TODO Auto-generated method stub 
-		String hql = " from Product";
-		Query query = sessionFactory.getCurrentSession().createQuery(hql);  //to add try catch right click surround with
-		return query.list();
-		
-	}
-	public boolean add(Product product) {
 		// TODO Auto-generated method stub
-		return false;
+		String hql = " from Product";
+		Query query = sessionFactory.getCurrentSession().createQuery(hql); // to
+																			// add
+																			// try
+																			// catch
+																			// right
+																			// click
+																			// surround
+																			// with
+		return query.list();
+
+	}
+
+	@Transactional
+	public boolean saveOrUpdate(Product product) {
+		// TODO Auto-generated method stub
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(product);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+
+		}
+
+	}
+
+	@Transactional
+	public Product getName(String name) {
+		// TODO Auto-generated method stub
+		System.out.println("Inside getproductByName daoimpl");
+		System.out.println("name:" + name);
+		String hql = "from Product where id= " + " ' " + name + " ' ";
+
+		Query query = sessionFactory.getCurrentSession().createQuery(hql);
+		List<Product> list = query.list();
+
+		if (list == null || list.isEmpty()) {
+			return null;
+		} else {
+			return list.get(0);
+		}
+
 	}
 
 }
