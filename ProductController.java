a93ec8e5186c4515e7c2a549818bf0eb4e13a56a
ANKITA MISHRA <mishrankita25@yahoo.com>
2016-09-13 13:08:56 +0530

    Add files via upload

--- ProductController.java
+++ ProductController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,52 +30,54 @@ import com.niit.model.Supplier;
 
 @Controller
 public class ProductController {
-	
+
 	@Autowired
 	Supplier supplier;
-	
+
 	@Autowired
 	Category category;
-	
+
 	@Autowired
 	Product product;
-	
+
 	@Autowired
 	ProductDAO productDAO;
-	
+
 	@Autowired
 	SupplierDAO supplierDAO;
-	
+
 	@Autowired
 	CategoryDAO categoryDAO;
 
 	@RequestMapping("/addproduct") // , method=RequestMethod.POST)
 	public ModelAndView showAddProduct(@Valid @ModelAttribute("prod") Product p1, BindingResult result,
-			HttpServletRequest request,HttpSession session) throws IOException {
-		
-		session.setAttribute("supplier",supplier);
-		session.setAttribute("supplierList",supplierDAO.list());
-		
-		session.setAttribute("category",category);
-		session.setAttribute("categoryList",categoryDAO.list());
+			HttpServletRequest request, HttpSession session) throws IOException {
+
+		// session.setAttribute("supplier",supplier);
+		session.setAttribute("supplierList", supplierDAO.list());
+
+		session.setAttribute("productList", productDAO.list());
+
+		// session.setAttribute("category",category);
+		session.setAttribute("categoryList", categoryDAO.list());
 		System.out.println("In add product page");
 		return new ModelAndView("addproduct");
 
 	}
-	
-	@SuppressWarnings("unchecked")
+
 	@RequestMapping("/registerP")
-	public String createProduct(@ModelAttribute("prod") Product p2, @RequestParam("image") MultipartFile file,@RequestParam("img") MultipartFile file1,
-			Model model, HttpServletRequest request) throws IOException {
-		productDAO.saveOrUpdate(p2);
-		
+	public String createProduct(@Valid @ModelAttribute("prod") Product p2, @RequestParam("image") MultipartFile file,
+			@RequestParam("img") MultipartFile file1, Model model, HttpServletRequest request) throws IOException {
+		// productDAO.saveOrUpdate(p2);
+
 		System.out.println("image");
 		MultipartFile image = p2.getImage();
 		Path path;
-		path = Paths.get("F://Single//Single//src//main//webapp//resources//images//" + p2.getPro_Id() + ".jpg");
+		path = Paths.get(
+				"F://Separate//LightingFrontEnd//src//main//webapp//resources//images//" + p2.getPro_Id() + ".jpg");
 		System.out.println("Path = " + path);
 		System.out.println("File name = " + p2.getImage().getOriginalFilename());
-		
+
 		if (image != null && !image.isEmpty()) {
 			try {
 				image.transferTo(new File(path.toString()));
@@ -84,12 +87,13 @@ public class ProductController {
 				System.out.println("First Image not saved");
 			}
 		}
-		
+
 		System.out.println("second image");
 		MultipartFile img = p2.getImg();
 		Path path1;
-		path1 = Paths.get("F://Single//Single//src//main//webapp//resources//images//" + p2.getProd_ID() + ".jpg");
-		
+		path1 = Paths.get(
+				"F://Separate//LightingFrontEnd//src//main//webapp//resources//images//" + p2.getProd_ID() + ".jpg");
+
 		if (img != null && !img.isEmpty()) {
 			try {
 				img.transferTo(new File(path1.toString()));
@@ -99,19 +103,61 @@ public class ProductController {
 				System.out.println("Second Image not saved");
 			}
 		}
-		Supplier supplier = supplierDAO.getName(((Supplier) product.getSupplier()).getSup_name());
+		 Supplier supplier = supplierDAO.getName(p2.getSupplier().getSup_name());
 		supplierDAO.saveOrUpdate(supplier);
 
-		Category category = categoryDAO.getName(product.getCategory().getCat_name());
+		 Category category = categoryDAO.getName(p2.getCategory().getCat_name());
 		categoryDAO.saveOrUpdate(category);
 
-		product.setCategory(category);
-		product.setSupplier((Set<Supplier>) supplier);
+		p2.setCategory(category);
+		p2.setSupplier(supplier);
+		// product.setSupplier((Set<Supplier>) supplier);
+		System.out.println("set");
+		
+		p2.setCat_id(category.getCat_id());
+		p2.setSup_id(supplier.getSup_id());		
+		
+		productDAO.saveOrUpdate(p2);
+		System.out.println("product added");
 
-		productDAO.saveOrUpdate(product);
+		model.addAttribute("message", "Products added successfully");
+		model.addAttribute("productList", productDAO.list());
 
-		return "adminhome";
+		return "addproduct";
 	}
 
+	@RequestMapping("product/remove/{id}")
+	public ModelAndView deleteProduct(@PathVariable("id") String id, Product p4, BindingResult result,
+			HttpServletRequest request) throws Exception {
+
+		p4 = productDAO.get(id);
+		ModelAndView mv = new ModelAndView("addproduct");
+		if (p4 == null) {
+			mv.addObject("errorMessage", "Could not delete");
+		} else {
+			productDAO.delete(p4);
+		}
+		mv.addObject("productList", productDAO.list());
+		return mv;
+	}
+
+	@RequestMapping("product/edit/{id}") // @ModelAttribute("category"),Category
+	// category
+	public String editCategory(@ModelAttribute("id") Product p5, BindingResult result,
+			HttpServletRequest request, Model model) {
+		System.out.println("in product editpage ");
 		
+		product = productDAO.get(p5.getPro_Id());
+		System.out.println(p5.getPro_Id());
+		if (category != null) {
+			productDAO.saveOrUpdate(p5);
+			model.addAttribute("message", "Successfully updated");
+		} else {
+			model.addAttribute("errorMessage", "Could not be updated");
+		}
+		model.addAttribute("product", product);
+		model.addAttribute("productList", productDAO.list());
+		return "redirect:/product";
+	}
+
 }
