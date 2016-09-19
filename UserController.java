b84f22157930f316d63f23cf79660e0c4126024f
ANKITA MISHRA <mishrankita25@yahoo.com>
2016-09-19 22:41:05 +0530

    Add files via upload

--- UserController.java
+++ UserController.java
@@ -1,6 +1,7 @@
 package com.niit.controller;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -9,16 +10,21 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.niit.dao.CartDAO;
 import com.niit.dao.CategoryDAO;
 import com.niit.dao.SupplierDAO;
 import com.niit.dao.UserDAO;
+import com.niit.model.Cart;
 import com.niit.model.Category;
 import com.niit.model.Supplier;
 import com.niit.model.User;
@@ -46,6 +52,13 @@ public class UserController {
 	@Autowired
 	User user;
 	
+	@Autowired
+	Cart cart;
+	
+	@Autowired
+	CartDAO cartDAO;
+	
+	
 	@RequestMapping("/signin")
 	public ModelAndView SignInPage() {
 		System.out.println("In SignIn page");
@@ -55,51 +68,86 @@ public class UserController {
 	}
 
 	
-	@RequestMapping("/check")
+	/*@RequestMapping("/check")
 	public ModelAndView login(@RequestParam("txtname")String userID,@RequestParam("txtpass")String password,HttpSession session){
         log.debug("Starting of the method login");
         log.info("userID is {} password is {}",userID,password);
         
         System.out.println("In validate sign in page "+userID+" "+password);
         
-        ModelAndView mv; 
+        ModelAndView mv=new ModelAndView("home");; 
         //boolean isValidUser=  userDAO.isValidUser(userID,password);
         
         if(userDAO.isValidUser(userID,password) == true){
         	user=userDAO.get(userID);
-        	//session.setAttribute("loggedInUser", user.getFirstname());
-        	//session.setAttribute("loggedInUserID", user.getUserID());
+        	session.setAttribute("loggedInUser", user.getName());
+        	session.setAttribute("loggedInUserID", user.getUserID());
         	
         	session.setAttribute("User", user);
         	
         	if(user.getRole().equals("ADMIN")){
-        		//mv.addObject("isAdmin", "true");
+          		mv.addObject("isAdmin", "true");
         		session.setAttribute("supplier",supplier);
         		session.setAttribute("supplierList",supplierDAO.list());
         		
         		session.setAttribute("category",category);
         		session.setAttribute("categoryList",categoryDAO.list());
-        		mv=new ModelAndView("adminhome");
+        		
            	}
         	else{
-        		//mv.addObject("isAdmin", "false");
-        		/*cart=cartDAO.get(UserID);
+        		mv.addObject("isAdmin", "false");
+        		cart=cartDAO.getCartByUserId(userID);
         		mv.addObject("cart", cart);
         		//Fetch the cartList based on USERID
-        		mv.addObject("cartList", cartList);
-        		mv.addObject("cartSize", cartList.size());*/
-        		mv=new ModelAndView("home1");
-        	}        		
+        		mv.addObject("cartList", cartDAO.listCart());
+        		mv.addObject("cartSize", cartDAO.listCart().size());
+              	}        		
         	}else{
-        		//mv.addObject("invalidCredentials", "true");
-        		//mv.addObject("errorMessage", "Invalid Credentials");
-        		mv=new ModelAndView("home1");
+        		mv = new ModelAndView("signin");
+        		mv.addObject("invalidCredentials", "true");
+        		mv.addObject("errorMessage", "Invalid Credentials!");
         }
         log.debug("Ending of method login");
         return mv;
 	}
+*/	
 	
-	@RequestMapping("/logout")
+	@RequestMapping(value="/login_session_attributes")
+	public String login_session_attributes(HttpSession session,Model model){
+		String name=SecurityContextHolder.getContext().getAuthentication().getName();
+		System.out.println("inside security check");
+		
+		session.setAttribute("name", name);
+		System.out.println(name);
+		
+		user=userDAO.get(name);
+		session.setAttribute("loggedInUser", user.getName());
+    	session.setAttribute("loggedInUserID", user.getUserID());
+    	
+		session.setAttribute("LoggedIn", "true");
+		
+		@SuppressWarnings("unchecked")
+		Collection<GrantedAuthority> authorities =(Collection<GrantedAuthority>) SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+		
+		String role="ROLE_USER";
+		for(GrantedAuthority authority : authorities)
+		{
+			if(authority.getAuthority().equals(role))
+			{
+				System.out.println(role);
+				return "home";
+			}
+			else
+			{
+				session.setAttribute("isAdmin", "true");
+			}
+			}
+		return "adminhome" ;
+		
+		
+	}
+	
+	@RequestMapping("/perform_logout")
 	public ModelAndView logout(HttpServletRequest request,HttpSession session){
 		ModelAndView mv=new ModelAndView("home");
 		session.invalidate();
@@ -107,8 +155,8 @@ public class UserController {
 		session.setAttribute("category", category);
 		session.setAttribute("categoryList", categoryDAO.list());
 		
-		mv.addObject("logOut Message", "You have successfully logged out");
-		mv.addObject("logged out","true");
+		mv.addObject("logOutMessage", "You have successfully logged out!");
+		mv.addObject("loggedout","true");
 		
 		return mv;
 }
@@ -124,8 +172,8 @@ public class UserController {
 	public ModelAndView ValidateSignUpPage(@Valid @ModelAttribute("us") User u, BindingResult result,
 			HttpServletRequest request) throws IOException {
 		System.out.println("In SignUp page");
-		u.setRole("USER");
-
+		u.setRole("ROLE_USER");
+		u.setEnabled(true);
 		System.out.println(u.getPassword());
 		System.out.println(u.getCpassword());
 		
